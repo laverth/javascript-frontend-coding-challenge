5fd0ac967932c1532dd35bec1af821666ab874c9
Pedro Knup <pk@MacBook-Pro-van-Pedro.local>
2019-09-22 22:30:28 +0200

    Implemented arrow navigation

--- Autocomplete.js
+++ Autocomplete.js
@@ -2,16 +2,52 @@ export default class Autocomplete {
   constructor(rootEl, options = {}) {
     options = Object.assign({ numOfResults: 10, data: [] }, options);
     Object.assign(this, { rootEl, options });
-
+    this.selectedIndex = -1;
+    document.addEventListener("keyup", this.checkKey.bind(this));
+    this.results = [];
     this.init();
+  }
+
+  checkKey(event) {
+    event = event || window.event;
+    if (event.keyCode == "38") {
+      // up arrow
+      this.decreaseIndex();
+    } else if (event.keyCode == "40") {
+      // down arrow
+      this.increaseIndex();
+    } else if (event.keyCode == "13") {
+      // enter
+      this.selectIndex();
+    }
+    this.updateDropdown();
+    console.log("current:", this.selectedIndex);
+  }
+
+  increaseIndex() {
+    if (this.selectedIndex < this.results.length - 1) {
+      this.selectedIndex += 1;
+    }
+  }
+
+  decreaseIndex() {
+    if (this.selectedIndex >= 0) {
+      this.selectedIndex -= 1;
+    }
+  }
+
+  selectIndex() {
+    if (this.selectedIndex >= 0 && this.selectedIndex < this.results.length) {
+      console.log(this.results[this.selectedIndex]);
+    }
   }
 
   onQueryChange(query) {
     // Get data for the dropdown
     let results = this.getResults(query, this.options.data);
-    results = results.slice(0, this.options.numOfResults);
-
-    this.updateDropdown(results);
+    this.results = results.slice(0, this.options.numOfResults);
+    this.selectedIndex = -1;
+    this.updateDropdown();
   }
 
   /**
@@ -21,31 +57,34 @@ export default class Autocomplete {
     if (!query) return [];
 
     // Filter for matching strings
-    let results = data.filter((item) => {
+    let results = data.filter(item => {
       return item.text.toLowerCase().includes(query.toLowerCase());
     });
 
+    console.log(results);
     return results;
   }
 
-  updateDropdown(results) {
-    this.listEl.innerHTML = '';
-    this.listEl.appendChild(this.createResultsEl(results));
+  updateDropdown() {
+    this.listEl.innerHTML = "";
+    this.listEl.appendChild(this.createResultsEl());
   }
 
-  createResultsEl(results) {
+  createResultsEl() {
     const fragment = document.createDocumentFragment();
-    results.forEach((result) => {
-      const el = document.createElement('li');
+    let index = -1;
+    this.results.forEach(result => {
+      index += 1;
+      const el = document.createElement("li");
       Object.assign(el, {
-        className: 'result',
-        textContent: result.text,
+        className: index === this.selectedIndex ? "selected-result" : "result",
+        textContent: result.text
       });
 
       // Pass the value to the onSelect callback
-      el.addEventListener('click', (event) => {
+      el.addEventListener("click", event => {
         const { onSelect } = this.options;
-        if (typeof onSelect === 'function') onSelect(result.value);
+        if (typeof onSelect === "function") onSelect(result.value);
       });
 
       fragment.appendChild(el);
@@ -54,15 +93,16 @@ export default class Autocomplete {
   }
 
   createQueryInputEl() {
-    const inputEl = document.createElement('input');
+    const inputEl = document.createElement("input");
     Object.assign(inputEl, {
-      type: 'search',
-      name: 'query',
-      autocomplete: 'off',
+      type: "search",
+      name: "query",
+      autocomplete: "off"
     });
 
-    inputEl.addEventListener('input', event =>
-      this.onQueryChange(event.target.value));
+    inputEl.addEventListener("input", event =>
+      this.onQueryChange(event.target.value)
+    );
 
     return inputEl;
   }
@@ -70,11 +110,11 @@ export default class Autocomplete {
   init() {
     // Build query input
     this.inputEl = this.createQueryInputEl();
-    this.rootEl.appendChild(this.inputEl)
+    this.rootEl.appendChild(this.inputEl);
 
     // Build results dropdown
-    this.listEl = document.createElement('ul');
-    Object.assign(this.listEl, { className: 'results' });
+    this.listEl = document.createElement("ul");
+    Object.assign(this.listEl, { className: "results" });
     this.rootEl.appendChild(this.listEl);
   }
 }
